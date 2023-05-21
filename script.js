b6ac40c15741b20fb0e4bc88c1dca0a313933257
GegaAxalaia <gega.axa@gmail.com>
2023-05-21 17:01:18 +0400

    added js file

---
+++ script.js
@@ -0,0 +1,28 @@
+var firstName = "luka"
+var lastName = "razikasvhili"
+var age = "54"
+var alive = "false"
+
+console.log(`my name is  ${firstName}  ${lastName} and I am ${age} years old.`);
+console.log(`he is alive: ${alive}`);
+
+let x = 14
+let y = 42
+let z = 2
+let c = 39
+let d = 33
+
+console.log(x + y + z - d)
+console.log(x / z + d + c + y / z)
+
+let rectangle = null;
+rectangle = {
+    oneSide: "8",
+    secondSide :"4",
+}
+console.log(
+    `if we want to calculate the area of a rectangle We must multiply ${rectangle.oneSide} by ${rectangle.secondSide}`
+);
+let q = 8
+let w = 4
+console.log(q * w)
